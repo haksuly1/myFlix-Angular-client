9e87e744d3d33298a1f13e35069164d38756662e
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-04-21 00:29:53 +0200

    implemented registration/login form

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,58 +1,68 @@
-/**
- * The UserLoginFormComponent is used to login users.
+/** 
+ * The UserLoginFormComponent is used to render a mat dialog containing a form where the
+ * user can submit their credentials to log in to myFlix.
  * @module UserLoginFormComponent
  */
 
 import { Component, OnInit, Input } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
-import { FetchApiDataService } from '../fetch-api-data.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
+// Used to navigate the user to the movies route on a successful login
 import { Router } from '@angular/router';
+// Used to access the loginUser function created on this service
+import { FetchApiDataService } from '../fetch-api-data.service';
+// Material Imports
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
-  styleUrls: ['./user-login-form.component.scss'],
+  styleUrls: ['./user-login-form.component.scss']
 })
 export class UserLoginFormComponent implements OnInit {
-  /**
-   * get input info and store it in userCredentials
+
+  /** 
+   * userCredentials values are populated by form inputs in the user-login-form template that are bound 
+   * using the ngModel directive.
    */
-  @Input() userCredentials = { Username: '', Password: '' };
+  @Input() userCredentials = {
+    Username: '',
+    Password: ''
+  };
 
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar,
-    public router: Router
+    public router: Router,
   ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+  }
 
   /**
-   * Login user via input field by using API endpoint
-   * And store the users data in localstorage
-   * @function userLogin
-   * @param loginData {object}
-   * @return users data in json format
+   * Invokes the userLogin method on the fetchApiData service, with the userCredentials from the form
+   * in order to log in the user. A successful login closes the form and navigates the user to the
+   * movies route. A popup is displayed confirming login success. If unsuccessful, a popup message
+   * asks the user to check their username and password.
    */
   loginUser(): void {
-    this.fetchApiData.userLogin(this.userCredentials).subscribe(
-      (response) => {
-        console.log(response);
-        localStorage.setItem('user', response.user.Username);
-        localStorage.setItem('token', response.token);
-        this.dialogRef.close();
-        this.snackBar.open('You are logged in', 'OK', {
-          duration: 2000,
-        });
-        this.router.navigate(['movies']);
-      },
-      (response) => {
-        this.snackBar.open(response, 'OK', {
-          duration: 2000,
-        });
-      }
-    );
+    this.fetchApiData.userLogin(this.userCredentials).subscribe((response) => {
+      console.log(response);
+      // Logic for a successful user login
+      localStorage.setItem('token', response.token);
+      localStorage.setItem('UserID', response.user._id);
+      localStorage.setItem('user', JSON.stringify(response.user));
+      console.log(response.user);
+      this.dialogRef.close(); // This will close the modal on success!
+      this.snackBar.open('User login successful!', 'OK', {
+        duration: 5000
+      });
+      this.router.navigate(['movies']);
+    }, (response) => {
+      this.snackBar.open("Login unsuccessful. Please check your username and password", 'OK', {
+        duration: 2000
+      });
+    });
   }
 }
--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,7 +1,4 @@
-/**
- * The UserRegistrationFormComponent is used to register a new user.
- * @module UserRegistrationFormComponent
- */
+// src/app/user-registration-form/user-registration-form.component.ts
 import { Component, OnInit, Input } from '@angular/core';
 
 // You'll use this import to close the dialog on success
@@ -13,51 +10,43 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+
 @Component({
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
-  styleUrls: ['./user-registration-form.component.scss'],
+  styleUrls: ['./user-registration-form.component.scss']
 })
 export class UserRegistrationFormComponent implements OnInit {
-  /**
-   * get input data of user and stores in userCredentials
-   */
-  @Input() userCredentials = {
-    Username: '',
-    Password: '',
-    Email: '',
-    Birthday: '',
-  };
+
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
 
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
-    public snackBar: MatSnackBar
-  ) { }
+    public snackBar: MatSnackBar) { }
 
-  ngOnInit(): void { }
-  /**
-   * call API endpoint to register a new user
-   * @function userRegistration
-   * @param userCredentials {object}
-   * @return new users data in json format
-   */
-  registerUser(): void {
-    this.fetchApiData.userRegistration(this.userCredentials).subscribe(
-      (response) => {
-        // Logic for a successful user registration goes here! (To be implemented)
-        this.dialogRef.close(); // This will close the modal on success!
-        console.log(response);
-        this.snackBar.open('user registered successfully', 'OK', {
-          duration: 2000,
-        });
-      },
-      (response) => {
-        console.log(response);
-        this.snackBar.open(response, 'OK', {
-          duration: 2000,
-        });
-      }
-    );
+  ngOnInit(): void {
   }
+
+  // This is the function responsible for sending the form inputs to the backend
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+      // Logic for a successful user registration goes here! (To be implemented)
+      this.dialogRef.close(); // This will close the modal on success!
+
+      console.log(Response);
+
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    }, (response) => {
+
+      console.log(response);
+
+      this.snackBar.open(response, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
 }
